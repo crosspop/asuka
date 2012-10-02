55750667ae13cd25e2ab4355616bad0cc04612eb
Hong Minhee <minhee@dahlia.kr>
2012-10-03 04:19:56 +0900

    Package structures and asuka.app module

---
+++ asuka/app.py
@@ -0,0 +1,87 @@
+""":mod:`asuka.app` --- Application configuration
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+import io
+
+from boto.ec2.connection import EC2Connection
+from paramiko.pkey import PKey
+from paramiko.rsakey import RSAKey
+
+
+class App(object):
+    """Application configuration.  It takes keyword-only parameters
+    that are the same name of settable properties and attributes.
+
+    """
+
+    #: (:class:`basestring`) The format string of AWS key pair name.
+    KEY_PAIR_NAME_FORMAT = 'Asuka-{app.name}'
+
+    #: (:class:`basestring`) The name of the app.
+    name = None
+
+    #: (:class:`boto.ec2.connection.EC2Connection`) The EC2 connection
+    #: to invoke APIs.
+    ec2_connection = None
+
+    def __init__(self, **values):
+        # Pop and set "name" and "ec2_connection" first because other
+        # properties require it.
+        try:
+            self.name = values.pop('name')
+        except KeyError:
+            raise TypeError('missing name parameter')
+        else:
+            if not isinstance(self.name, basestring):
+                raise TypeError('name must be a string, not ' +
+                                repr(self.name))
+        try:
+            self.ec2_connection = values.pop('ec2_connection')
+        except KeyError:
+            raise TypeError('missing ec2_connection parameter')
+        else:
+            if not isinstance(self.ec2_connection, EC2Connection):
+                raise TypeError('ec2_connection must be an instance of '
+                                'boto.ec2.connection.EC2Connection, not ' +
+                                repr(self.ec2_connection))
+        # For all keys: self.$key = values['$key']
+        for attr, value in values.iteritems():
+            setattr(self, attr, value)
+        if self.private_key is None:
+            self.key_pair
+
+    @property
+    def private_key(self):
+        """(:class:`paramiko.pkey.PKey`) The pair of public and private key."""
+        return getattr(self, '_private_key', None)
+
+    @private_key.setter
+    def private_key(self, pkey):
+        if not isinstance(pkey, PKey):
+            raise TypeError('private_key must be an instance of paramiko.'
+                            'pkey.PKey, not ' + repr(pkey))
+        self._private_key = pkey
+        name = self.KEY_PAIR_NAME_FORMAT.format(app=self)
+        keys = self.ec2_connection.get_all_key_pairs([name])
+        if keys:
+            key_pair = keys[0]
+        else:
+            pub = ' '.join((pkey.get_name(), pkey.get_base64(), name))
+            key_pair = self.ec2_connection.import_key_pair(name, pub)
+        self._key_pair = key_pair
+
+    @property
+    def key_pair(self):
+        """(:class:`boto.ec2.keypair.KeyPair`) The EC2 key pair matched to
+        :attr:`private_key`.
+
+        """
+        try:
+            return self._key_pair
+        except AttributeError:
+            self._key_pair = self.ec2_connection.create_key_pair(
+                self.KEY_PAIR_NAME_FORMAT.format(app=self)
+            )
+            private_key = str(self._key_pair.material)
+            self._private_key = RSAKey.from_private_key(io.BytesIO(private_key))
---
+++ setup.py
@@ -0,0 +1,37 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+from asuka.version import VERSION
+
+
+install_requires = [
+    'boto == 2.6.0', 'paramiko == 1.7.7.2',
+]
+
+
+def readme():
+    try:
+        with open('README.rst') as f:
+            return f.read()
+    except IOError:
+        pass
+
+
+setup(
+    name='Asuka',
+    packages=find_packages(),
+    install_requires=install_requires,
+    version=VERSION,
+    description='A deployment system for Python web apps using GitHub and EC2',
+    long_description=readme(),
+    author='Hong Minhee',
+    author_email='dahlia' '@' 'crosspop.in',
+    maintainer='Hong Minhee',
+    maintainer_email='dahlia' '@' 'crosspop.in',
+    url='https://github.com/crosspop/asuka',
+    license='MIT License'
+)
