ecb5cb20b84396c33d04c1c5d58e9ec09b642236
Hong Minhee <minhee@dahlia.kr>
2012-10-10 16:59:22 +0900

    Upload docs to gh-pages

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+import distutils.cmd
+import os
+import os.path
+import shutil
+import tempfile
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
@@ -20,6 +26,34 @@ def readme():
             return f.read()
     except IOError:
         pass
+
+
+class upload_doc(distutils.cmd.Command):
+    """Uploads the documentation to GitHub pages."""
+
+    description = __doc__
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        path = tempfile.mkdtemp()
+        build = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             'build', 'sphinx', 'html')
+        os.chdir(path)
+        os.system('git clone git@github.com:crosspop/asuka.git .')
+        os.system('git checkout gh-pages')
+        os.system('git rm -r .')
+        os.system('touch .nojekyll')
+        os.system('cp -r ' + build + '/* .')
+        os.system('git stage .')
+        os.system('git commit -a -m "Documentation updated."')
+        os.system('git push origin gh-pages')
+        shutil.rmtree(path)
 
 
 setup(
