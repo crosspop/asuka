7610725692dd6d1296120f11febe3c30f63b264e
Hong Minhee <dahlia@crosspop.in>
2012-10-26 22:30:40 +0000

    Logging on asuka-server

--- asuka/cli.py
+++ asuka/cli.py
@@ -2,8 +2,10 @@
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+import logging
 import optparse
 import os.path
+import sys
 
 from waitress import serve
 from werkzeug.wsgi import DispatcherMiddleware
@@ -23,6 +25,10 @@ def run_server():
                       help='Port to listen [default: %default]')
     parser.add_option('--pong', help='Path which simply responds 200 OK e.g. '
                                      '--pong=/ping/')
+    parser.add_option('--log-file', default='/dev/stderr',
+                      help='File to write logs [default: %default]')
+    parser.add_option('-v', '--verbose', action='store_true', default=False)
+    parser.add_option('-q', '--quiet', action='store_true', default=False)
     options, args = parser.parse_args()
     try:
         config, = args
@@ -32,6 +38,9 @@ def run_server():
         parser.error('missing config file')
     if not os.path.isfile(config):
         parser.error('cannot read ' + config)
+    if options.verbose and options.quiet:
+        parser.error('options -v/--verbose and -q/--quiet are mutually '
+                     'exclusive')
     app = app_from_config_file(config)
     webapp = WebApp(app)
     def pong(environ, start_response):
@@ -39,4 +48,17 @@ def run_server():
         return ['pong']
     if options.pong:
         webapp = DispatcherMiddleware(webapp, {options.pong: pong})
+    if options.log_file in ('/dev/stderr', '/dev/stdout'):
+        log_file = getattr(sys, options.log_file[:-6])
+    else:
+        log_file = open(options.log_file, 'a')
+    if options.quiet:
+        logging_level = logging.ERROR
+    elif options.verbose:
+        logging_level = logging.DEBUG
+    else:
+        logging_level = logging.INFO
+    logger =  logging.getLogger()
+    logger.setLevel(logging_level)
+    logger.addHandler(logging.StreamHandler(log_file))
     serve(webapp, host=options.host, port=options.port)
