5167a19a81f8eb37241df570f1cd596fd61b03b8
Hong Minhee <dahlia@crosspop.in>
2013-03-19 22:46:45 +0000

    Link to service config file of the commit

--- asuka/urls.py
+++ asuka/urls.py
@@ -3,9 +3,20 @@
 
 """
 
-__all__ = 'repository',
+__all__ = 'repository', 'service_config_file'
 
 
 def repository(app):
     """GitHub repository."""
     return app.repository.html_url
+
+
+def service_config_file(deployment, service):
+    """The :file:`{service}.yml` config file of the commit."""
+    branch = deployment.branch
+    commit = deployment.commit
+    path = branch.app.config_dir
+    if not path.endswith('/'):
+        path += '/'
+    path += service + '.yml'
+    return branch.repository.contents(path, ref=commit.ref).html_url
